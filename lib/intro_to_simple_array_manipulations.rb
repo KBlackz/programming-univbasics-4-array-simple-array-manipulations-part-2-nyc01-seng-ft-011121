455f4eeae1e2ac87022fa547ea35408fbe395606
Karris  Black <karris.b@gmail.com>
2020-12-31 01:20:53 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,10 @@
+def using_concat
+    more_favs = ["sports cars", "flatiron school"]
+    all_my_favs.concat(["jim", "call"])
+
+
+end
+
+
+
+[ "a", "b" ].concat( ["c", "d"] ) #=> [ "a", "b", "c", "d" ]
